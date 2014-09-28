83b20a929af9932f2ec9772f40ea64fb3f967a2e
Henry Xie <xie1989@gmail.com>
2014-09-28 10:13:39 -0400

    flickr basic

--- pkg/flickr_basic/script.js
+++ pkg/flickr_basic/script.js
@@ -6,22 +6,42 @@ var indexCounter = 0;
 var container = $('#container');
 var viewport = $('#viewport');
 
+
+function fitToViewport(element){
+  if(element) {
+    element.style.width = viewport.width() + "px";
+  }
+}
+
+
 function showImage(index){
   // whats the offset? hint: use the image number and the viewport width
 
   var viewportWidth = -$(window).width();
 
   // What's the offset
+  var offset = index * viewportWidth;
+
   // Use the offset as the margin-left
+  container.css('margin-left', offset);
+
 }
 
 function nextImage(){
 
   // if indexcounter is the same as imagecount
-  // reset the indexcounter to 0
+  if (indexCounter === imageCount) {
 
-  // increment the indexcounter
-  // call showImage with the indexCounter
+    // reset the indexcounter to 0
+    indexCounter = 0;
+  }
+  else {
+    // increment the indexcounter
+    indexCounter++;
+
+    // call showImage with the indexCounter
+    showImage(indexCounter);
+  }
 }
 
 function apiSuccess(result){
@@ -29,6 +49,16 @@ function apiSuccess(result){
   console.log(photos);
 
   // For photo in photos
+  for (var i = 0; i < photos.length; i++) {
+    var photo = photos[i];
+    var img = new Image();
+    img.src = photo['media']['m'].replace('_m', '_b');
+    var post = $('<div class="post"></div>');
+    fitToViewport(post[0]);
+    post.append(img);
+    container.append(post);
+
+  }
 
 }
 
