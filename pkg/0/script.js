483303170d3363f71abaadf2b93d3804a177bc18
Henry Xie <xie1989@gmail.com>
2014-09-28 09:17:44 -0400

    simple functions with for loops

--- pkg/0/script.js
+++ pkg/0/script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+// Print all numbers between 10 and 30 (inclusive) that are evenly divisible by 3.
+
+
+
+function range(start, end) {
+    var list = [];
+    for (var i = start; i <= end; i++) {
+        list.push(i);
+    }
+    return list;
+}
+
+
+function looping(divisor) {
+    var items = range(10, 30);
+
+    for (var i = 0; i < items.length; i++) {
+        var number = items[i];
+        if (number % divisor === 0) {
+            console.log(number);
+        }
+    }
+}
